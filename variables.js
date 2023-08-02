e9707150bd68ef7395317928363c9b7740c105b5
jorgeduje <jorgeduje4@gmail.com>
2023-08-02 00:06:22 -0300

    se trabaja con variables tipos de datos y operadores

---
+++ variables.js
@@ -0,0 +1,89 @@
+// ANCHOR VARIABLES ---> let y const
+
+let edad = 25;
+const apellido = "perez";
+
+console.log(edad);
+console.log(apellido);
+
+edad = edad + 1;
+
+console.log(edad);
+
+// LINK ESTO NO SE PUEDE POR QUE ES UNA CONSTANTE
+// apellido = "otra cosa"
+// console.log(apellido)
+
+// ANCHOR ---> TIPOS DE DATOS ---> primitivos
+
+// STRING ---> cadena de texto
+let nombre = "pepe"
+
+// NUMBER ---> numeros
+let numeroDeLaSuerte = 22
+
+// BOOLEAN ---> true - false
+let isLogged = true
+
+// UNDEFINED 
+let nombreDeUsuario = undefined
+
+console.log(nombreDeUsuario)
+
+
+// NULL
+let datos = null
+
+// NaN --> not a number
+
+let x = 1
+let y = "pepe"
+
+let z = x * y
+
+console.log(z)
+
+// ANCHOR ---> OPERADORES
+
+// LINK ---> matematicos
+
+let n1 = 13
+let n2 = 6
+
+console.log( n1 + n2)
+console.log( n1 - n2)
+console.log( n1 * n2)
+console.log( n1 / n2)
+// MODULO %
+console.log( n1 % n2 )
+
+// CONCATENAR ---> JUNTAR 
+
+
+let saludo = "hola" + " " + nombre + " " + "como estas?"
+
+console.log(saludo)
+
+// TEMPLATE LITERALS ---> BACKSTICKS
+let saludoBien = `hola ${nombre} como estas?` 
+console.log(saludoBien)
+
+
+// LINK ---> comparacion ---> SIEMPRE DEVUELVE UN BOOLEANO
+
+let numeroUno = 5 
+let numeroDos = "5"
+// console.log( numeroUno == numeroDos) // ---> simple
+// console.log( numeroUno != numeroDos) // ---> simple
+
+console.log( numeroUno === numeroDos) // ---> estricta
+console.log( numeroUno !== numeroDos) // ---> estricta
+// console.log( numeroUno > numeroDos)
+// console.log( numeroUno < numeroDos)
+// console.log( numeroUno >= numeroDos)
+// console.log( numeroUno <= numeroDos)
+
+
+
+
+// LINK ---> logicos
