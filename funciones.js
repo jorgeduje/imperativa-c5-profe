e792c9d38b89071257488051e0576654ed351cc6
jorgeduje <jorgeduje4@gmail.com>
2023-08-09 00:06:35 -0300

    se trabaja con funciones

--- funciones.js
+++ funciones.js
@@ -1,3 +1,8 @@
+// HOISTING
+
+
+// ANCHOR ---> FUNCIONES
+// FUNCIONES DECLARADAS VS EXPRESADAS
 
 // console.log( "paso adelante")
 // console.log( "paso al costado derecho")
@@ -16,7 +21,8 @@
 // console.log( "salto")
 // console.log( "prende el foco ")
 
-
+// DECLARADAS ---> TIENEN NOMBRE
+let saludoFinal = saludar( "perez")
 function saludar ( nombre, apellido ){
    
     let saludo = `Hola ${nombre} ${apellido} como estas?`
@@ -24,21 +30,21 @@ function saludar ( nombre, apellido ){
 
 }
 
-
-let saludoFinal = saludar( "perez")
-console.log(saludoFinal)
-
-let numeroUno = 4 // lo sacaremos de lo que ingrese el usuario
-let numeroDos = 3 // lo sacaremos de lo que ingrese el usuario
-
-
-
 function sumar ( n1, n2 ){
 
     return n1 + n2
 }
 
+console.log(saludoFinal)
+// console.log( saludar( "perez") )
+
+let numeroUno = 4 // lo sacaremos de lo que ingrese el usuario
+let numeroDos = 3 // lo sacaremos de lo que ingrese el usuario
+
+
 let res = sumar(numeroUno, numeroDos)
+
+
 
 
 
@@ -47,3 +53,75 @@ let res = sumar(numeroUno, numeroDos)
 
 // let res3 = sumar(8, 1)
 // console.log(res3)
+
+// EXPRESADAS ----> ANONIMAS ( 2 tipos , regular o de tipo flecha)
+// let edad =2 
+
+// let nombre = "pepe"
+
+const restar =  function (a, b){
+    return a - b
+}
+
+let resultadoResta = restar(10, 4)
+console.log(resultadoResta)
+
+// MULTIPLICAR ---> por 5
+// ARROW FUNCTION
+
+
+
+const multiplicarPorCinco =  (numero)=>{
+   
+let palabra = "casa"
+  
+    return numero * 5
+
+}
+
+let resultadoMulti = multiplicarPorCinco( 3 )
+console.log(resultadoMulti)
+
+
+// SCOPE ---> alcance o ambiente o lugar donde existe o donde vive  una variable 
+
+// SCOPE ----> local 
+
+// {
+// x ---> no 
+//     {
+//         let x = 5
+//         x ---> si
+
+//         {
+//             x ---> si
+
+
+//         }
+
+//         x ---> si
+//     }
+//     x ---> no
+// }
+
+// x ---> no
+
+
+let palabra = "casa"
+
+const pruebaScope = ()=>{
+    let palabra = "avion"
+   return palabra
+}
+
+pruebaScope()
+
+
+// NECESITO UNA FUNCION QUE SIRVA PARA CONVERTIR PESOS A DOLARES 
+// Y QUE LOS PESOS SEAN DINAMICOS Y QUE EL PRECIO DEL DOLAR TAMBIEN
+
+const convertir = (cantidadPesos, precioDolar)=> cantidadPesos / precioDolar
+
+
+let resultadoDolares = convertir(5000, 200)
+console.log(resultadoDolares)
